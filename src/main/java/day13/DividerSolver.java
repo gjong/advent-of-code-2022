b53875f08e6ea181d7eb96f793d1db0dd3999a6a
Gerben Jongerius <g.jongerius@jong-soft.com>
2022-12-13 15:13:40 +0100

    Optimize day 13 part 2.

--- src/main/java/day13/DividerSolver.java
+++ src/main/java/day13/DividerSolver.java
@@ -2,35 +2,27 @@ package day13;
 
 import common.Solution;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class DividerSolver implements Solution<Scanner, Integer> {
     @Override
     public Integer solve(Scanner input) {
-        List<DataPacket> packets = new ArrayList<>();
+        var smallerDiv2 = 1;
+        var smallerDiv6 = 2; // starts at 2 because [[2]] is also in the list
+
+        var dataPacketDiv2 = DataPacket.of("[[2]]");
+        var dataPacketDiv6 = DataPacket.of("[[6]]");
         while (input.hasNextLine()) {
             var line = input.nextLine();
             if (line.isEmpty()) {
                 continue;
             }
 
-            packets.add(DataPacket.of(line));
+            var packet = DataPacket.of(line);
+            if (packet.compareTo(dataPacketDiv2) < 0) smallerDiv2++;
+            if (packet.compareTo(dataPacketDiv6) < 0) smallerDiv6++;
         }
 
-        packets.add(DataPacket.of("[[2]]"));
-        packets.add(DataPacket.of("[[6]]"));
-
-        var stringify = packets.stream()
-                .sorted(DataPacket::compareTo)
-                .map(Objects::toString)
-                .toList();
-
-        var idxSep2 = stringify.indexOf("[[2]]") + 1;
-        var idxSep6 = stringify.indexOf("[[6]]") + 1;
-
-        return idxSep2 * idxSep6;
+        return smallerDiv2 * smallerDiv6;
     }
 }
